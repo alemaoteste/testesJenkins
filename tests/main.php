cab0c93422d58bfde44a82bfc5c7bc669586e1fa
Tiago <tsantarosa@gmail.com>
2012-09-18 17:19:30 -0300

    taloko!

--- tests/main.php
+++ tests/main.php
@@ -1,8 +1,8 @@
 <?php
 define ( 'TESTS_PATH',  dirname(__FILE__) . DIRECTORY_SEPARATOR );
 require_once( TESTS_PATH . 'simpletest/autorun.php');
-require_once( TESTS_PATH . 'simpletest/reporter.php');
-require_once( TESTS_PATH . 'simpletest/xml.php');
+//require_once( TESTS_PATH . 'simpletest/reporter.php');
+//require_once( TESTS_PATH . 'simpletest/xml.php');
 //require_once( TESTS_PATH . '../simpletest/unit_tester.php');
 //require_once( TESTS_PATH . '../simpletest/reporter.php');
 //require_once( TESTS_PATH . '../simpletest/xml.php');
@@ -13,5 +13,5 @@ class AllTests extends TestSuite {
         $this->addFile( TESTS_PATH . 'teste1.php');
     }
 }
-$at = new AllTests();
-$at->run(new XMLReporter());
+//$at = new AllTests();
+//$at->run(new XMLReporter());
